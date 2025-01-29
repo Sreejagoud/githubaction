a7ab52ec209590436d551b01ec711d33dc65cd6c
Sreejagoud <102413780+Sreejagoud@users.noreply.github.com>
2025-01-29 21:39:44 +0530

    Update index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,6 @@
 const greet = require('../src/utils');
 
 test('greet function should return a greeting message', () => {
-  expect(greet('Narender')).toBe('Hello, Alice!');
-  expect(greet('Sreeja')).toBe('Hello, Bob!');
+  expect(greet('Narender')).toBe('Hello, Narender!');
+  expect(greet('Sreeja')).toBe('Hello, Sreeja!');
 });
